94c1fdfc649e158ed6f9bd6cc0a9f96b870309f3
Thomas Winters <winterst85@gmail.com>
2018-07-22 22:34:11 -0500

    rough draft of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,94 @@
-//Choose Theme
-//use key events for letter choosen by user
-/* 
-Display on html page:
-1 - Press any key to get started.
-2 - Wins - # of times user guessed word correctly
-    Start the first word with _ _ _ _ based on number of letters
-    as user guesses a letter, reveal spot where letter is
-3 - Display number of guesses remaining
-4 - Display the letters choosen
-5 - after user wins/loses - repeat with next word and make player play again
-*/
+//Var to hold words for guessing
+var wordGuess = ["eclair", "flan", "ladyfingers", "truffle", "shortcake"];
+//variables
+var numberTries = 15;
+var userLetters = [];
+var currentWordIndex;
+var userGuessingWord = [];
+var remainingGuesses = 0;
+var gameEnded = false;
+var wins = 0;
 
-/* 
-Psuedo Code:
-Html :
-    current word
-     _ _ _ _ _ - on document load?
-CSS : design
-JS :
-array - words in game
-When user selects a key
-    add to number of guesses
-    replace _ if letter is there
-    add letter to selected list
-        make sure repeat guesses do not affect html
-Repeat until word is complete OR remaining guesses = 0
-    Then start new word
-    Repeat
-*/
+function resetGame() {
+    remainingGuesses = numberTries;
+
+    currentWordIndex = Math.floor(Math.random() * (wordGuess.length));
+
+    userLetters = [];
+    userGuessingWord = [];
+
+    for (var i = 0; i < wordGuess[currentWordIndex].length; i++) {
+        userGuessingWord.push("_");
+    }
+    updateDisplay();
+};
+
+function updateDisplay() {
+    document.getElementById("userWins").innertext = wins;
+    
+    var userGuessingWordText = "";
+
+    for (var i = 0; i < userGuessingWord.length; i++) {
+        userGuessingWordText += userGuessingWord[i];
+    }
+    document.getElementById("gameWord").innerText = userGuessingWordText;
+    document.getElementById("guessRemain").innerText = remainingGuesses;
+    document.getElementById("lettersGuessed").innerText = userLetters;
+    document.getElementById("userWins").innerText = wins;
+};
+
+function userGuess(letter) { 
+    if (remainingGuesses > 0) {
+        if (userLetters.indexOf(letter) === -1) {
+            userLetters.push(letter);
+            evaluateGuess(letter);
+        }
+    }
+};
+
+function evaluateGuess(letter) {
+    var positions = [];
+    for (var i = 0; i < wordGuess[currentWordIndex].length; i++) {
+        if (wordGuess[currentWordIndex][i] === letter) {
+            positions.push(i);
+        }
+    }
+
+    if (positions.length <= 0) {
+        remainingGuesses--;
+    }
+    else {
+        for (var i = 0; i < positions.length; i++) {
+            userGuessingWord[positions[i]] = letter;
+        }
+    }
+};
+
+function checkWin() {
+    if (userGuessingWord.indexOf("_") === -1) {
+        wins++;
+        gameEnded = true;
+    }
+};
+
+function checkLoss()
+{
+    if(remainingGuesses <= 0) {
+       gameEnded = true;
+    }
+}
+
+document.onkeydown = function(event) {
+    if (gameEnded) {
+        resetGame();
+        gameEnded = false;
+    }
+    else { 
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+        userGuess(event.key.toLowerCase());
+        updateDisplay();
+        checkWin();
+        checkLoss();
+    }
+}
+};
